187e67ff5c913a649f1232b15bde6d5a8579a360
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-31 23:42:23 -0500

    server tests complete for post and delete

--- api/server.js
+++ api/server.js
@@ -9,4 +9,14 @@ server.get("/", async (req, res) => {
   res.status(200).send("hi there");
 });
 
+server.post("/", async (req, res) => {
+  let id = await dbHelper.insert(req.body);
+  res.status(201).json(id);
+});
+
+server.delete("/", async (req, res) => {
+  let count = await dbHelper.remove(req.body);
+  res.status(200).json(count);
+});
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,10 +1,49 @@
 const request = require("supertest");
 const server = require("./server.js");
 
+const db = require("../dbConfig");
+
+// before and after Each or before and after All
+afterEach(async () => {
+  await db("resources").truncate();
+});
+
 describe("server.js", () => {
   it("GET should respond with status 200", async () => {
     let response = await request(server).get("/");
 
     expect(response.status).toBe(200);
+  });
+  it("POST should respond with status 201", async () => {
+    let response = await request(server)
+      .post("/")
+      .send({ name: "Lumber" });
+
+    expect(response.status).toBe(201);
+  });
+  it("POST should return number representing id", async () => {
+    let response = await request(server)
+      .post("/")
+      .send({ name: "Lumber" });
+
+    expect(typeof response.body).toBe("number");
+  });
+  it("DELETE should respond with status 200", async () => {
+    await request(server)
+      .post("/")
+      .send({ name: "Lumber" });
+    let response = await request(server)
+      .delete("/")
+      .send({ name: "Lumber" });
+    expect(response.status).toBe(200);
+  });
+  it("DELETE should remove 1 resource", async () => {
+    await request(server)
+      .post("/")
+      .send({ name: "Lumber" });
+    let response = await request(server)
+      .delete("/")
+      .send({ name: "Lumber" });
+    expect(response.body).toBe(1);
   });
 });
